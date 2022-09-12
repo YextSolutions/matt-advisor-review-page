7db4646a23c7feeffbc227ab0403c0faa9ff7e66
mattmalone212 <79661550+mattmalone212@users.noreply.github.com>
2022-09-12 13:39:54 -0400

    Update and rename createEntity.ts to sendTestimonialToFunction.ts

---
+++ src/utils/sendTestimonialToFunction.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+type testimonialSubmission = {
+  authorName: string;
+  authorEmail: string;
+  title: string;
+  rating: 1 | 2 | 3 | 4 | 5;
+  content: string;
+  reviewLabelNames: string[];
+  invitationUid?: string;
+};
+
+
+export const sendTestimonialToFunction = (testimonial: testimonialSubmission) => {
+  // Get curent date as YYYY-MM-DD
+  // const reviewDate = new Date().toISOString().split('T')[0];
+
+  const data = { ...testimonial};
+
+  console.log('Posting Following Review', data);
+  return axios.get ('https://huma4jczdk-41836-d.preview.pagescdn.com/functions/api/serverless/createTestimonial.ts', {
+    params: {
+      title: testimonial.title,
+      author: testimonial.authorName,
+      authorEmail: testimonial.authorEmail,
+      content: testimonial.content,
+      labels: testimonial.reviewLabelNames
+    },
+  });
+};
+
+export default createEntity;
