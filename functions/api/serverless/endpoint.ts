a67ef5dc7869909aeaf5d7beb52f650038872c85
mattmalone212 <79661550+mattmalone212@users.noreply.github.com>
2022-09-12 13:24:41 -0400

    Update endpoint.ts

--- functions/api/serverless/endpoint.ts
+++ functions/api/serverless/endpoint.ts
@@ -1,1 +1,33 @@
-// this is where we are going to POST to the K-api
+export async function main(argumentJson) {
+   let url = argumentJson["requestUrl"];
+   let questionMark = url.indexOf("?");
+   url = url.substring(questionMark);
+   let urlParams = new URLSearchParams(url);
+   let title = urlParams.get("title");
+   let author = urlParams.get("author");
+   let authorEmail = urlParams.get("authorEmail");
+   let content = urlParams.get("content");
+   let labels = urlParams.get("labels");
+   let key = "7579d93a8ebdcbe477e3f59f50376a04";
+ 
+   const postUrl = 'https://api.yext.com/v2/accounts/me/entities?api_key=' + key + '&entityType=ce_testimonial' + '&v=20220808';
+   let data = {
+        "name": title,
+        "c_author": author,
+        "c_authorEmail": authorEmail,
+        "c_content" : content,
+        "c_labels" : labels
+       }
+   
+    const response = await fetch(postUrl, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify(data),
+      });
+return null
